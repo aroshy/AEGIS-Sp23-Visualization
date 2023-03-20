5b711c884ef15d42d158f1414754404a6c9cb668
emily-tonkovich <124398064+emily-tonkovich@users.noreply.github.com>
2023-03-20 12:45:13 -0400

    Add files via upload

--- ImportCSV.py
+++ ImportCSV.py
@@ -1,32 +1,15 @@
-
 import csv
-'''
+nodesNames = []
 with open("R2_1247_3_t11_ResidentialVoltages_new.csv", 'r') as file:
   csvreader = csv.reader(file)
-  row_count = sum(1 for row in csvreader)
-  y = []
-  for index, row in enumerate(csvreader):
-    for x in range(9, row_count):
-      if index == x:
-        y.append(row[1])
-        #print(row[1])
-
-print(row_count)
-'''
-with open("R2_1247_3_t11_ResidentialVoltages_new.csv", 'r') as file:
-  csvreader = csv.reader(file)
-  #row_count = sum(1 for row in csvreader)
-  #col_count = 
-  y = []
-  x = []
-  n = 0
-  for index, row in enumerate(csvreader):
-      for n in range(0, 100):
-        if index == 8:
-          y.append(row[n])
-        if index == 9:
-          x.append(row[n])
-          n = n + 1
-  #for index, row in enumerate(csvreader):
-print(y[0])
-print(x[0])
+  row_count = 0 
+  data = []
+  for j, row in enumerate(csvreader):
+    row_count = row_count + 1
+    if (j == 8):
+      nodesNames = row
+    if (j >= 9):
+      data.append(row)
+    #print(row_count)
+print(nodesNames[0:20])
+print(data[1][0:20])
