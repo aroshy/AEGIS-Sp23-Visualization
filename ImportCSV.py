924bece5c534a6c7188ac1c60a15076a2021faad
emily-tonkovich <124398064+emily-tonkovich@users.noreply.github.com>
2023-03-20 12:46:08 -0400

    Update ImportCSV.py

--- ImportCSV.py
+++ ImportCSV.py
@@ -11,5 +11,5 @@ with open("R2_1247_3_t11_ResidentialVoltages_new.csv", 'r') as file:
     if (j >= 9):
       data.append(row)
     #print(row_count)
-print(nodesNames[0:20])
-print(data[1][0:20])
+#print(nodesNames[0:20])
+#print(data[1][0:20])
